fb9e31f6ee204ae7489775229fafced4b3b3111e
Maksim Tarasyuk <mtarasyuk@agranovsky.org>
2020-08-25 13:37:31 +0300

    ProductTable.js added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,30 @@ import ProductTable from './components/ProductTable';
 
 import './App.css';
 
+const data = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
+// const data = [
+//     {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+//     {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+//     {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+//     {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+//     {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'},
+//     {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'}
+// ];
+
 function App() {
     return (
-        <ProductTable/>
+        <ProductTable
+            data={data}
+            inStockOnly={false}
+        />
     );
 }
 
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,22 +1,22 @@
 import React from 'react';
 
-const data = [
-    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
-
 function SearchBar(props) {
     return (
         <div className="search-bar">
             <label>
-                <input type="search" placeholder="Search..." onChange={props.onChange}/>
+                <input
+                    type="search"
+                    placeholder="Search..."
+                    onChange={props.onInputChange}
+                    value={props.value}
+                />
             </label>
             <label>
-                <input type="checkbox"/>
+                <input
+                    type="checkbox"
+                    checked={props.inStockOnly}
+                    onChange={props.onCheckboxChange}
+                />
                 Only show products in stock
             </label>
         </div>
@@ -41,9 +41,17 @@ function Table(props) {
 
 function TableEntry(props) {
     return (
-        <tr>
-            <td>{props.name}</td>
-            <td>{props.price}</td>
+        <tr className={!props.product.stocked ? 'not-stocked' : ''}>
+            <td>{props.product.name}</td>
+            <td>{props.product.price}</td>
+        </tr>
+    );
+}
+
+function EntryCategory(props) {
+    return (
+        <tr className="category-name">
+            <td colSpan="2">{props.category}</td>
         </tr>
     );
 }
@@ -51,22 +59,85 @@ function TableEntry(props) {
 export default class ProductTable extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {searchVal: ''};
+        this.state = {
+            searchVal: '',
+            inStockOnly: props.inStockOnly
+        };
     }
 
     handleSearchChange = (e) => {
         this.setState({searchVal: e.target.value});
+    };
+
+    handleCheckboxChange = () => {
+        this.setState({inStockOnly: !this.state.inStockOnly});
+    };
+
+    generateRows () {
+        this.categories = [];
+        this.dataByCategories = {};
+        this.props.data.forEach(entry =>
+            this.categories.indexOf(entry.category) === -1 && this.categories.push(entry.category)
+        );
+        this.categories.forEach(category =>
+            this.dataByCategories[category] = this.props.data.filter(entry => category === entry.category)
+        );
+        let row = [];
+        for (let category of Object.keys(this.dataByCategories)) {
+                // @todo Delete EntryCategory when nothing in it
+                row.push(<EntryCategory key={category} category={category}/>);
+                const entries = this.dataByCategories[category]
+                    .filter(product => product.name.indexOf(this.state.searchVal) !== -1)
+                    .map(product => {
+                        return this.state.inStockOnly
+                            ? product.stocked && <TableEntry key={product.name} product={product}/>
+                            : <TableEntry key={product.name} product={product}/>
+                    });
+                entries.forEach(entry => row.push(entry));
+        }
+        return row;
     }
 
     render() {
+        // const filterText = this.props.filterText;
+        // const inStockOnly = this.props.inStockOnly;
+        //
+        // const rows = [];
+        // let lastCategory = null;
+        //
+        // this.props.products.forEach((product) => {
+        //     if (product.name.indexOf(filterText) === -1) {
+        //         return;
+        //     }
+        //     if (inStockOnly && !product.stocked) {
+        //         return;
+        //     }
+        //     if (product.category !== lastCategory) {
+        //         rows.push(
+        //             <ProductCategoryRow
+        //                 category={product.category}
+        //                 key={product.category} />
+        //         );
+        //     }
+        //     rows.push(
+        //         <ProductRow
+        //             product={product}
+        //             key={product.name}
+        //         />
+        //     );
+        //     lastCategory = product.category;
+        // });
+
         return (
             <div className="product-table">
                 <SearchBar
                     value={this.state.searchVal}
-                    onChange={this.handleSearchChange}
+                    inStockOnly={this.state.inStockOnly}
+                    onInputChange={this.handleSearchChange}
+                    onCheckboxChange={this.handleCheckboxChange}
                 />
                 <Table>
-                    {data.map(entry => <TableEntry key={[entry.name, entry.price].join().toLocaleLowerCase()} name={entry.name} price={entry.price}/>)}
+                    {this.generateRows()}
                 </Table>
             </div>
         );
